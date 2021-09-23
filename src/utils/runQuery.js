4e6d193e88b39268b1744d09c53ec6d1180e8145
LegrandMichael <michael.c.legrand@gmail.com>
2021-09-23 18:01:17 +0200

    [misc]chore: Retrait du try catch

--- src/utils/runQuery.js
+++ src/utils/runQuery.js
@@ -1,10 +1,6 @@
 import { createTables, insertIntoTables } from './queryFunction';
 
 (async () => {
-    try {
-        await createTables();
-        await insertIntoTables();
-    } catch (e) {
-        console.log(e);
-    }
+    await createTables();
+    await insertIntoTables();
 })();
